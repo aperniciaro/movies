4dcbf5f6e6ec56bf3e45e0dd7a106407a50827ba
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-20 17:58:20 -0500

    sorted by release date

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react'
 import Movie from './Movie'
 import data from '../1989.json'
 
+console.log(data.results)
+
+data.results.sort(function(a, b) {
+  if (a.release_date < b.release_date) {
+    return -1
+  } else if (a.release_date > b.release_date_) {
+    return 1
+  } else {
+    return 0
+  }
+})
+
 class Gallery extends Component {
   render() {
     return (
