9dcbb6eac3800ff218e016366fd2b9b665e0abbc
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-20 14:02:21 -0500

    created general html structure and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import Header from './components/Header'
+import Gallery from './components/Gallery'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    ;<section>
+      <header>
+        <h1>1989 Top Movies</h1>
+      </header>
+      <main>
+        <h2>title</h2>
+        <img>poster_path</img>
+        <p>overview</p>
+      </main>
+    </section>
   }
 }
 
