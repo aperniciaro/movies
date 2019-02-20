8af6dfc0c575e087080cf381f90ca3d8d163fe85
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-20 18:42:39 -0500

    moved images to src

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
+import filmPic from '../images/film.jpg'
 
 class Header extends Component {
   render() {
     return (
       <header>
-        <img src="/images/film.jpg" alt="film stuff" />
+        <img src={filmPic} alt="film stuff" />
         <h1>1989's Best Movies</h1>
-        <img src="/images/film.jpg" alt="film stuff" />
+        <img src={filmPic} alt="film stuff" />
       </header>
     )
   }
