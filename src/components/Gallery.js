bdde11c74effa8b64eb8541db2792b4f63c8b905
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-20 14:43:58 -0500

    page displayed correctly; needs styling

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import Movie from './Movie'
+import data from '../1989.json'
 
 class Gallery extends Component {
   render() {
     return (
       <main>
-        <Movie />
+        {data.results.map(movie => {
+          return (
+            <Movie
+              key={movie.id}
+              movieName={movie.title}
+              moviePoster={movie.poster_path}
+              movieOverview={movie.overview}
+            />
+          )
+        })}
       </main>
     )
   }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,11 +4,15 @@ class Movie extends Component {
   render() {
     return (
       <article>
-        <h2>title</h2>
-        {/* <img src="" alt="">
-          poster_path
-        </img> */}
-        <p>overview</p>
+        <h2>{this.props.movieName}</h2>
+        <img
+          src={
+            'https://image.tmdb.org/t/p/w185_and_h278_bestv2' +
+            this.props.moviePoster
+          }
+          alt="movie poster"
+        />
+        <p>{this.props.movieOverview}</p>
       </article>
     )
   }
